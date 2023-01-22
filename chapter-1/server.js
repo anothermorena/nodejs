d279b5ea151ec82344010e0315cc9f8aced5d5c7
anothermorena <hireme@morena.dev>
2023-01-22 15:27:25 +0200

    Chapter 1 intro uptp 2 points

---
+++ chapter-1/server.js
@@ -0,0 +1,4 @@
+//How NodeJS differs from Vanilla JS
+//1) Node runs on a server - not in a browser(backend not frontend)
+//2) The console is the terminal window not dev tools like when usng JS for the frontend
+console.log("Hello World");
